502de49ab5b0cf2cd82594772ed797353c7d2f92
hurb <hbreeze@student.42.fr>
2025-04-26 17:17:50 +0100

    added refernce for smallest sort cases

--- src/sorting/sort_five.c
+++ src/sorting/sort_five.c
@@ -6,11 +6,52 @@
 /*   By: hbreeze <hbreeze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 15:08:42 by hbreeze           #+#    #+#             */
-/*   Updated: 2025/04/26 14:20:14 by hbreeze          ###   ########.fr       */
+/*   Updated: 2025/04/26 17:17:35 by hbreeze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
+
+
+
+// Need to re-implement this code:
+
+/*
+void	sort_three(t_stack **head)
+{
+	int	biggest;
+
+	biggest = find_biggest(*head);
+	if ((*head)->index == biggest)
+		rotate_a(head);
+	else if ((*head)->next->index == biggest)
+		r_rotate_a(head);
+	if ((*head)->index > (*head)->next->index)
+		swap_a(head);
+}
+
+void	sort_five(t_stack **stack_a, t_stack **stack_b)
+{
+	int	size;
+
+	size = count_nodes(*stack_a);
+	while (size--)
+	{
+		if ((*stack_a)->index == 0 || (*stack_a)->index == 1)
+			push_b(stack_a, stack_b);
+		else
+			rotate_a(stack_a);
+	}
+	sort_three(stack_a);
+	push_a(stack_a, stack_b);
+	push_a(stack_a, stack_b);
+	if ((*stack_a)->index > (*stack_a)->next->index)
+		swap_a(stack_a);
+}
+
+void	radix
+*/
+
 
 void	a_sort_five(t_push_swap *meta)
 {
